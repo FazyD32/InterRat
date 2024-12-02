a522a6b1919bfee92ee8f86c1e50c998455dbcff
FazyD32 <fazyd32@mail.ru>
2024-12-02 19:04:57 +0200

    modified:   InterRatApp.kv 	modified:   main.py 	modified:   main.py

--- main.py
+++ main.py
@@ -107,6 +107,22 @@ class BaseControl:
             INSERT INTO UserCat (title) 
             VALUES ("Администратор")
                         ''')
+            
+            cursor.execute('''
+            INSERT INTO UserCat (title) 
+            VALUES ("Клиент")
+                        ''')
+            
+            cursor.execute('''
+            INSERT INTO PCCategories (title) 
+            VALUES ("Игровой")
+                        ''')
+            
+            cursor.execute('''
+            INSERT INTO Models (title) 
+            VALUES ("Игровой компьютер SmartDigital "1STPLAYER", процессор Intel i7, 32GB DDR4", 56183)
+                        ''')
+            
             connect.commit()
             connect.close()
         except Exception:
@@ -150,17 +166,17 @@ class UsersTable(MDGridLayout):
                                 ''').fetchall()
             self.table = MDDataTable(size_hint=(1, 1),
                                      pos_hint = {'center_x': 0.5,'y': 0.4},
-                                    rows_num = 40,
+                                    rows_num = 1000,
                                     column_data = [('id', 24), ('ФИО', self.width - 24)],
                                     row_data = [(e[0], e[1]) for e in self.data])
             self.add_widget(self.table)
-            self.table.bind(on_row_press=self.print_id)
+            self.table.bind(on_row_press=self.print_usertable)
         except Exception:
             with open('baseerr.txt', 'a') as file:
                 file.write(f'{datetime.datetime.now()} Ошибка отображения таблицы пользователей \n')
 
 
-    def print_id(self, instance_table, instance_row):
+    def print_usertable(self, instance_table, instance_row):
         id = (instance_row.text)
 
         if id in '0123456789':
@@ -169,11 +185,44 @@ class UsersTable(MDGridLayout):
             self.data = cursor.execute('''
             SELECT * FROM Users WHERE id = ? LIMIT 1
                            ''', (id,)).fetchall()
+            self.app = MDApp.get_running_app()
+            self.ids = self.app.root.ids.user_screen_id.ids
+            self.ids.user_id.text = str(self.data[0][0])
+            self.ids.user_namefull.text = str(self.data[0][1])
+            self.ids.birthday.text = str(self.data[0][2])
+            self.ids.user_login.text = str(self.data[0][3])
+            self.ids.user_password.text = str(self.data[0][4])
+            user_categ = cursor.execute('''
+            SELECT title FROM UserCat 
+            WHERE id = ? LIMIT 1
+                                        ''', (str(self.data[0][5]))).fetchall()
+            self.ids.user_cat.text = str(user_categ[0][0])
             print(*self.data, id)
 
             connect.commit()
             connect.close()
         
+
+class UsersScreenAdd(Screen):
+    def add_user_to_base(self, fullname, birthday, login, password):
+        try:
+            connect = sqlite3.connect('InterCof.db')
+            cursor = connect.cursor()
+
+            cursor.execute('''
+            INSERT INTO Users (fullname, birthday, login, password, cat_id)
+            VALUES (?, ?, ?, ?, ?)
+                        ''', (fullname, birthday, login, password, '2'))
+            self.manager.current = 'users'
+
+            connect.commit()
+            connect.close()
+        except Exception:
+            with open('baseerr.txt', 'a') as file:
+                file.write(f'{datetime.datetime.now()} Ошибка добавления пользователя \n')
+
+
+
 
 class InterRatApp(MDApp):
     def __init__(self, **kwargs):
@@ -184,11 +233,12 @@ class InterRatApp(MDApp):
     
 
     def build(self):
+        self.theme_cls.primary_palette = 'Amber'
         return MyMainLayout()
 
 
     def open_drawer(self, *arg):
-       self.root.navdrawer.set_state('open')
+       self.root.ids.nav_drawer.set_state('open')
 
 
     def enter(self, login, password, label, manager):
